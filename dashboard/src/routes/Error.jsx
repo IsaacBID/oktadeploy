4a0492c147fab271f4f439c8a3375b1b479dfe33
josias-serna <josias.serna@beyondid.com>
2024-02-16 17:54:10 -0600

    Added app layout

--- dashboard/src/routes/Error.jsx
+++ dashboard/src/routes/Error.jsx
@@ -4,8 +4,9 @@ export default function Error() {
     const { error } = useRouteError();
     return (
         <div>
-            <h1>{error.statusText}</h1>
-            <p>{error.message}</p>
+            {error == undefined && <h1>Unknown error</h1>}
+            {error.statusText != undefined && <h1>{error.statusText}</h1>}
+            {error.message != undefined && <p>{error.message}</p>}
         </div>
     );
 }
--- dashboard/src/routes/Root.jsx
+++ dashboard/src/routes/Root.jsx
@@ -1,10 +1,38 @@
 import { Outlet } from "react-router-dom";
+import { AppShell, Burger, Group } from "@mantine/core";    
+import { useDisclosure } from "@mantine/hooks";
 
 export default function Root() {
+    const [opened, { toggle }] = useDisclosure();
+
     return (
-        <div>
-        <h1>Root</h1>
-            <Outlet />
-        </div>
+        <AppShell
+            header={{ height: 54 }}
+            navbar={{ width: 200, breakpoint: "sm", collapsed:{mobile: !opened} }}
+            footer={{ height: 32}}
+            layout="alt"
+        >
+
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                    <h3>Okta Deploy Dashboard</h3>
+                </Group>
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">
+                menu
+            </AppShell.Navbar>
+
+            <AppShell.Main>
+                <h1>Dashboard</h1>
+                <Outlet />
+            </AppShell.Main>
+
+            <AppShell.Footer ta="center">
+                BeyondID &copy; {new Date().getFullYear()}
+            </AppShell.Footer>
+
+        </AppShell>
     )
 }
