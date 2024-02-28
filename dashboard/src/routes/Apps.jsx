a048b0fc07a516b88216b0cba078fbaf99f9105b
josias-serna <josias.serna@beyondid.com>
2024-02-28 14:47:27 -0600

    Added selected prop to each app to control the checkbox toggle

--- dashboard/src/routes/Apps.jsx
+++ dashboard/src/routes/Apps.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { applist as testAppList } from '../testdata';
-import { IconSearch } from '@tabler/icons-react';
+import { IconSearch, IconSettings } from '@tabler/icons-react';
 
 export default function Apps() {
     const redirect = useNavigate();
@@ -11,15 +11,27 @@ export default function Apps() {
     const [search, setSearch] = useState('');
 
     useEffect(() => {
-        setAppList(testAppList)
+        //request list of apps
+        
+        //adds selected prop to each app 
+        const appsWithSelectedProp = testAppList.map(app => ({ ...app, checked: true }));
+        setAppList(appsWithSelectedProp)
     }, []);
 
     useEffect(() => {
+        //make this validation routerlevel
         const tenant = localStorage.getItem('OriginTenant');
         if (tenant == null || tenant == undefined) {
             redirect('/');
         }
     }, []);
+
+    const toggleSelected = (index) => {
+        setAppList(appList.map((app, i) => 
+            i === index? { ...app, checked: !app.checked } : app
+        ));
+        console.log('toggled', appList[index].label)
+    }
 
     return (
         <>
@@ -46,9 +58,10 @@ export default function Apps() {
                             p="md"
                             gap="xs"
                         >
-                            <img src={app._links.logo[0].href} alt={app.label} width="24" height="24" />
+                            {app._links.logo === undefined ? <Box w={24} h={24}><IconSettings /></Box>:
+                            <img src={app._links.logo[0].href} alt={app.label} width="24" height="24" />}
                             <Text style={{whiteSpace:"nowrap", overflow:"hidden", textOverflow:"ellipsis"}}>{app.label}</Text>
-                            <Checkbox ml="auto" defaultChecked />
+                            <Checkbox ml="auto" checked={app.checked} onChange={() => toggleSelected(index)} />
                         </Flex>
                     )}
                 </SimpleGrid>
